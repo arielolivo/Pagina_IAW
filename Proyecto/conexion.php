6ba572a83f8dd91ebf4425d99601db25e9e69db1
Ariel_Olivo <ariel.olivo.yepez@iesciudadjardin.com>
2020-02-14 10:04:05 +0100

    orden de documentos

--- Proyecto/conexion.php
+++ Proyecto/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new mysqli("localhost", "root", "", "prueba");
+$mysqli = new mysqli("localhost", "root", "", "olivogonzalez");
 
 if($mysqli->connect_errno) {
 	echo "Falló la conexion a la base de datos";
